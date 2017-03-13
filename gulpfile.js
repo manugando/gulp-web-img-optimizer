9cc86d8193d5a5d782249eba6bd485a124f29a14
manugando <emanuele.gandolfi89@gmail.com>
2017-03-13 16:21:34 +0100

    Basic task implementation

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var del = require('del');
+var imagemin = require('gulp-imagemin');
+var imageminPngquant = require('imagemin-pngquant');
+var imageminJpegoptim = require('imagemin-jpegoptim');
+
+gulp.task('clean:img', function() {
+	return del.sync(['dist']);
+});
+
+gulp.task('build:img', ['clean:img'], function() {
+    return gulp.src('src/*.*')
+        .pipe(imagemin(
+                [
+                    imageminPngquant({speed: 1}), 
+                    imageminJpegoptim({max : 90})
+                ],
+                {verbose : true}
+            ))
+        .pipe(gulp.dest('dist/img'));
+});
